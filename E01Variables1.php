c835442f512124a38aec8f7f89a119c5b7ef1295
David-Jaquez <davricojaqu@gmail.com>
2024-10-21 20:51:35 -0600

    Add files via upload

---
+++ E01Variables1.php
@@ -0,0 +1,26 @@
+<?php 
+/* CBTIS 89
+Programa que Declara variables y las muestra
+Jaquez Almaguer
+3ºA Programaciòn matutino
+*/
+$dia = 24; //Se declara una variable tipo integer.
+$sueldo = 758.43; //Se declara una variable de tipo double.
+$nombre = " Pedrito"; //Se declara una variable tipo string.
+$exite = true; //Se declara una variable boolean.
+
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "Variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable string:";
+echo $nombre;echo "<br>";
+echo "Variable boolean:";
+echo $exite;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "Mundo";
+$cadena3= "Omigosh";
+echo $cadena1. "     ".$cadena2. " ".$cadena3;
+
+?>
